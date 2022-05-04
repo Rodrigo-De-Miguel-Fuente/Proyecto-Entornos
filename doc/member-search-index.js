0ef774e896a72a4470724cdd57315c569982ff9c
Rodrigo-De-Miguel-Fuente <99647951+Rodrigo-De-Miguel-Fuente@users.noreply.github.com>
2022-05-04 23:27:24 +0200

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Compra","c":"Compra","l":"Compra()","u":"%3Cinit%3E()"},{"p":"Compra","c":"Compra","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"Compra","c":"Compra","l":"oferta(float, float)","u":"oferta(float,float)"}];updateSearchResults();
